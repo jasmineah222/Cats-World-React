8f447a57ea8bf842133dd0df8172c5d0f482670e
jasmineah222 <jasmineah222@gmail.com>
2024-02-04 16:36:33 -0500

    Responsiveness being added to Single Cat page. Repsonsiveness had been added to Home page.

--- Cats World/src/App.jsx
+++ Cats World/src/App.jsx
@@ -1,5 +1,7 @@
 import { Link, Route, Routes} from 'react-router-dom'
+import './index.css'
 import './App.css'
+import './assets/icon.png' //just in case b/c app is top level
 import AllCats from './components/AllCats'
 import SingleCat from './components/SingleCat'
 import Compare from './components/Compare'
@@ -8,7 +10,7 @@ import NavBar from './components/NavBar'
 function App() {
 
   return (
-    <>
+    <div className="app">
       
       <NavBar/>
 
@@ -19,7 +21,9 @@ function App() {
         <Route path='/details/:id' element={<SingleCat/>}/>
         
       </Routes>
-    </>
+
+      <footer className='name'>@JH Organization</footer>
+    </div>
   )
 }
 
